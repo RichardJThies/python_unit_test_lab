bb34ce41438ddc59caf3fcbd152b8221cb37ba6b
SalvLav <richardjthies95@gmail.com>
2023-09-17 23:34:10 -0500

    nothing major

--- cart_discount/price_discount.py
+++ cart_discount/price_discount.py
@@ -1,6 +1,6 @@
 def main():
-
-    print(discount([10, 4, 20]))  # Expect this to print 4
+    list_of_item_prices = [10, 4, 20]
+    discount(list_of_item_prices)  # Expect this to print 4
     # what other lists might this function be called with?
     
 
@@ -8,9 +8,33 @@ def discount(item_prices):
     """ Complete this function that returns the discount earned for a list of item prices
     If a customer has ordered more than three items, the cheapest item is free.
     Example: if this function is called with a list of [10, 4, 20] then return 4.
-    """
 
-    pass  # todo replace this line with your code 
+    if no discount will be taken, return 0
+    What appropriate if function is called with weird/invalid data?
+    """
+    # numberofitems = len(item_prices)
+
+    # cheapest_item = min(item_prices)
+    #print(sorted(item_prices))
+    if len(item_prices) > 3:
+        for i in item_prices:
+            i = min(item_prices)
+            return i
+
+    elif len(item_prices) < 3:
+        return 0
+    
+    if item_prices == None:
+        raise ValueError('Need list')
+    # elif len(item_prices) == 0:
+    #     raise ValueError('No items in cart.')
+    
+    else:
+        return None
+    
+    
+    
+    # pass  # todo replace this line with your code 
 
 
 if __name__ == '__main__':
--- cart_discount/test_discount.py
+++ cart_discount/test_discount.py
@@ -9,9 +9,29 @@ class TestDiscount(TestCase):
         expected_discount = 4
         self.assertEqual(expected_discount, discount(prices))
 
-    
-    # TODO more unit tests here. Each test should test one scenario
+    # def test_list_of_less_than_three_prices(self):
+    #     self.assertEqual(None,discount([10, 4]))
 
+    # def test_list_of_less_with_one_price(self):
+    #     self.assertEqual(10, discount([10, 10, 10,]))
+
+    # def test_list_of_two_prices_no_discount(self):
+    #     prices = [20, 1]#arrange
+    #     expexted_discount = 0#arrange
+    #     actual_discount = discount(prices)#action
+    #     self.assertEqual(expexted_discount, actual_discount)#assert
+
+    
+    # def test_list_of_less_than_three_items(self.)
+    #     self.fail('test')
+    
+    # def test_list
+
+
+    # def test_rturnswhencalledwith1item(self):
+    #     self.fail('Finish this test')
+    # # TODO more unit tests here. Each test should test one scenario
+    # #what if function is called isnt a list or a weird data type?
 
 if __name__ == '__main__':
     unittest.main()
